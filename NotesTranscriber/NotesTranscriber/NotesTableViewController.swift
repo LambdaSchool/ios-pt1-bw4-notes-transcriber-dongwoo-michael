67688fcbed6df87e782aeb0740e49b4f1d62fdd6
Dongwoo Pae <dongwoopae@gmail.com>
2019-12-20 13:08:23 +0900

    added tableview datasurce and prepare segue

--- NotesTranscriber/NotesTranscriber/NotesTableViewController.swift
+++ NotesTranscriber/NotesTranscriber/NotesTableViewController.swift
@@ -10,81 +10,61 @@ import UIKit
 
 class NotesTableViewController: UITableViewController {
 
+    let noteController = NoteController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+     override func viewWillAppear(_ animated: Bool) {
+           super.viewWillAppear(animated)
+           self.tableView.reloadData()
+       }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.noteController.notes.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let note = self.noteController.notes[indexPath.row]
+        cell.textLabel?.text = note.noteText
+        
+        //Date Formatter
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy HH:mm:ss"
+        let dateFromNote = note.timestamp
+        let createdDate = dateFormatter.string(from: dateFromNote!)
+        dateFormatter.timeZone = NSTimeZone.local
+        
+        cell.detailTextLabel?.text = createdDate
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let note = self.noteController.notes[indexPath.row]
+            self.noteController.deleteNote(for: note)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "AddText" {
+            guard let destVC = segue.destination as? TextViewViewController else {return}
+        
+        } else if segue.identifier == "UpdateText" {
+            guard let destVC = segue.destination as? TextViewViewController,
+                let selectedRow = self.tableView.indexPathForSelectedRow else {return}
+            
+        }
     }
-    */
-
 }
