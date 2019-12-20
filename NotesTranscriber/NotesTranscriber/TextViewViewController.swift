bdc0765b6cb7fe6da9ed6fb36cbbd5cb0771cfdc
Dongwoo Pae <dongwoopae@gmail.com>
2019-12-20 13:02:03 +0900

    set up storyboards and connected outlets

---
+++ NotesTranscriber/NotesTranscriber/TextViewViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TextViewViewController.swift
+//  NotesTranscriber
+//
+//  Created by Dongwoo Pae on 12/20/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import UIKit
+
+class TextViewViewController: UIViewController {
+
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        
+    }
+}
