bfa49ffafabc5d95561c3b882d6712b2377711af
Dongwoo Pae <dongwoopae@gmail.com>
2020-01-07 16:12:30 +0900

    added protocol and delegate between tableview and cell for sharebutton with activitycontroller

--- NotesTranscriber/NotesTranscriber/ViewControllers/NotesTableViewController.swift
+++ NotesTranscriber/NotesTranscriber/ViewControllers/NotesTableViewController.swift
@@ -8,34 +8,36 @@
 
 import UIKit
 
-class NotesTableViewController: UITableViewController {
-
+class NotesTableViewController: UITableViewController, NoteTableViewCellDelegate {
+    
+    
     let noteController = NoteController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
-     override func viewWillAppear(_ animated: Bool) {
-           super.viewWillAppear(animated)
-           self.tableView.reloadData()
-       }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tableView.reloadData()
+    }
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.noteController.notes.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NoteTableViewCell
         let note = self.noteController.notes[indexPath.row]
         cell.note = note
+        cell.delegate = self
         return cell
     }
-
-
+    
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -44,9 +46,9 @@ class NotesTableViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
-
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ToAddNote" {
@@ -59,4 +61,15 @@ class NotesTableViewController: UITableViewController {
             destVC.note = self.noteController.notes[selectedRow.row]
         }
     }
+    
+    
+    func showActivityView(for cell: NoteTableViewCell) {
+        guard let noteInput = cell.note?.noteText else {
+            print("no text found")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [noteInput], applicationActivities: [])
+        self.present(vc, animated: true, completion: nil)
+    }
+    
 }
