b2b66e0b82223d71ad12b07651ae2cbd4b40cf40
Dongwoo Pae <dongwoopae@gmail.com>
2020-01-07 15:57:41 +0900

    added extension for UIButton animation

--- NotesTranscriber/NotesTranscriber/ViewControllers/NoteTableViewCell.swift
+++ NotesTranscriber/NotesTranscriber/ViewControllers/NoteTableViewCell.swift
@@ -12,7 +12,7 @@ class NoteTableViewCell: UITableViewCell {
 
     @IBOutlet weak var noteLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
-    
+    @IBOutlet weak var shareButton: UIButton!
     
     var note: Note? {
         didSet {
@@ -45,6 +45,28 @@ class NoteTableViewCell: UITableViewCell {
     
     
     @IBAction func shareButtonTapped(_ sender: Any) {
+        self.shareButton.shake()
+    }
+    
+}
+
+extension UIButton {
+    func shake() {
         
+        let shake = CABasicAnimation(keyPath: "position")
+        shake.duration = 0.1
+        shake.repeatCount = 2
+        shake.autoreverses = true
+        
+        let fromPoint = CGPoint(x: center.x - 3, y: center.y)
+        let fromValue = NSValue(cgPoint: fromPoint)
+        
+        let toPoint = CGPoint(x: center.x + 3, y: center.y)
+        let toValue = NSValue(cgPoint: toPoint)
+        
+        shake.fromValue = fromValue
+        shake.toValue = toValue
+        
+        layer.add(shake, forKey: nil)
     }
 }
